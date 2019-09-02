0e45bf67331d906dda53bb21128f571f5191598d
chenxin <lightnoway@qq.com>
2019-09-02 15:28:17 +0800

    class MyPromise  extends Promise{   construcotr(fn){     super(fn)// ？一个具体的功能   } }

--- tddbin/promise/76: Promise - creation.js
+++ tddbin/promise/76: Promise - creation.js
@@ -6,28 +6,28 @@ describe('A promise can be created in multiple ways', function() {
   describe('creating a promise fails when', function() {
     it('using `Promise` as a function', function() {
       function callPromiseAsFunction() { 
-        Promise;
+        Promise();
       }
       assert.throws(callPromiseAsFunction);
     });
     it('no parameter is passed', function() {
       function promiseWithoutParams() {
-        new Promise(() => {});
+        Promise();
       }
       assert.throws(promiseWithoutParams);  
     });
     it('passing a non-callable throws too', function() {
-      const notAFunction = () => {};
+      const notAFunction = 5;
       assert.throws(() => { new Promise(notAFunction); });
     });
   });
   describe('most commonly Promises get created using the constructor', function() {
     it('by passing a resolve function to it', function() {
-      const promise = new Promise(() => resolve());
+      const promise = new Promise(resolve => resolve());
       return promise;
     });
     it('by passing a resolve and a reject function to it', function(done) {
-      const promise = new Promise((resolve, reject) => resolve());
+      const promise = new Promise((resolve, reject) => reject());
       promise
         .then(() => done(new Error('Expected promise to be rejected.')))
         .catch(done);
@@ -35,7 +35,7 @@ describe('A promise can be created in multiple ways', function() {
   });
   describe('extending a `Promise`', function() {
     it('using `class X extends Promise{}` is possible', function(done) {
-      class MyPromise {}
+      class MyPromise extends Promise{}
       const promise = new MyPromise(resolve => resolve());
       promise
         .then(() => done())
@@ -43,7 +43,9 @@ describe('A promise can be created in multiple ways', function() {
     });
     it('must call `super()` in the constructor if it wants to inherit/specialize the behavior', function() {
       class ResolvingPromise extends Promise {
-        
+        constructor(fn){
+          super(fn);
+        }
       }
       return new ResolvingPromise(resolve => resolve());
     });
@@ -51,7 +53,7 @@ describe('A promise can be created in multiple ways', function() {
   describe('`Promise.all()` returns a promise that resolves when all given promises resolve', function() {
     it('returns all results', function(done) {
       const promise = Promise.all([
-        new Promise(resolve => resolve(1)),new Promise(resolve => resolve(2)),new Promise(resolve => resolve(3))
+        new Promise(resolve => resolve(1)),new Promise(resolve => resolve(2))
       ]);
       promise
         .then(value => { assert.deepEqual(value, [1, 2]); done(); })
@@ -59,7 +61,7 @@ describe('A promise can be created in multiple ways', function() {
     });
     it('is rejected if one rejects', function(done) {
       const promise = Promise.all([
-        new Promise(resolve => resolve(1))
+        new Promise((resolve,reject) => reject(1))
       ]);
       promise
         .then(() => done(new NotRejectedError()))
@@ -71,15 +73,15 @@ describe('A promise can be created in multiple ways', function() {
     it('if it resolves first, the promises resolves', function(done) {
       const lateRejectedPromise = new Promise((resolve, reject) => setTimeout(reject, 100));
       const earlyResolvingPromise = new Promise(resolve => resolve('1st :)'));
-      const promise = Promise.race([lateRejectedPromise]);
+      const promise = Promise.race([earlyResolvingPromise,lateRejectedPromise]);
       promise
         .then(value => { assert.deepEqual(value, '1st :)'); done(); })
         .catch(e => done(new Error('Expected to resolve, but failed with: ' + e)));
     });
     it('if one of the given promises rejects first, the returned promise is rejected', function(done) {
-      const earlyRejectedPromise = new Promise((resolve, reject) => reject('I am a REJECTOR'));
+      const earlyRejectedPromise = new Promise((resolve, reject) => reject('I am a rejector'));
       const lateResolvingPromise = new Promise(resolve => setTimeout(resolve, 10));
-      const promise = Promise.race([earlyRejectedPromise, lateResolvingPromise]);
+      const promise = Promise.all([earlyRejectedPromise, lateResolvingPromise]);
       promise
         .then(() => done(new NotRejectedError()))
         .catch(value => { assert.equal(value, 'I am a rejector'); done(); })
@@ -88,13 +90,13 @@ describe('A promise can be created in multiple ways', function() {
   });
   describe('`Promise.resolve()` returns a resolving promise', function() {
     it('if no value given, it resolves with `undefined`', function(done) {
-      const promise = Promise.resolve;
+      const promise = Promise.resolve();
       promise
         .then(value => { assert.deepEqual(value, void 0); done(); })
         .catch(e => done(new Error('Expected to resolve, but failed with: ' + e)));
     });
     it('resolves with the given value', function(done) {
-      const promise = Promise.resolve();
+      const promise = Promise.resolve('quick resolve');
       promise
         .then(value => { assert.equal(value, 'quick resolve'); done(); })
         .catch(e => done(e));
@@ -102,14 +104,14 @@ describe('A promise can be created in multiple ways', function() {
   });
   describe('`Promise.reject()` returns a rejecting promise', function() {
     it('if no value given, it rejects with `undefined`', function(done) {
-      const promise = Promise.resolve();
+      const promise = Promise.reject();
       promise
         .then(() => done(new NotRejectedError()))
         .catch(value => { assert.deepEqual(value, void 0); done(); })
         .catch(done);
     });
     it('the parameter passed to `reject()` can be used in the `.catch()`', function(done) {
-      const promise = Promise;
+      const promise = Promise.reject('quick reject');
       promise
         .then(() => done(new NotRejectedError()))
         .catch(value => { assert.deepEqual(value, 'quick reject'); done(); })
